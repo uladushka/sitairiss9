b544c538b75a139b70654eb15f586f268e3486d9
uladushka <vl.lovets@gmail.com>
2024-12-04 20:42:55 +0300

    Initial commit

---
+++ src/org/sitairiss9/Main.java
@@ -0,0 +1,7 @@
+package org.sitairiss9;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is the sitairiss9 Java project using Ant.");
+    }
+}
